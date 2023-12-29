b81933cff2aaa434f88824845819edc93810fc67
JustBadrun <128286595+JustBadrun@users.noreply.github.com>
2023-12-29 20:23:51 +0700

    Update

--- Jobsheet 1/A. GPIO/Mengendalikan Led Menggunakan Push Button/Mengendalikan Led Menggunakan Push Button.ino
+++ Jobsheet 1/A. GPIO/Mengendalikan Led Menggunakan Push Button/Mengendalikan Led Menggunakan Push Button.ino
@@ -1,7 +1,3 @@
-// 08 September 2023
-// Alfan, Ayum, Rani
-// Program Mengendalikan LED menggunakan Push Button
-
 // mengatur posisi PIN
 const int button1 = 4;  // button1 dihubungkan pada pin GPIO4
 const int button2 = 6;  // button2 dihubungkan pada pin GPIO6
