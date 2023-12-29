12306b115fc499873adfc80a47805476c4e49af5
JustBadrun <128286595+JustBadrun@users.noreply.github.com>
2023-12-29 20:24:33 +0700

    Update

--- Jobsheet 1/A. GPIO/Running Led/Running Led.ino
+++ Jobsheet 1/A. GPIO/Running Led/Running Led.ino
@@ -1,7 +1,3 @@
-// 10 September 2023
-// Alfan, Ayum, Rani
-// Program Running LED
-
 // mengatur posisi PIN
 const int button1 = 4;    // button1 dihubungkan pada pin GPIO4
 const int button2 = 2;  // button2 dihubungkan pada pin GPIO5
